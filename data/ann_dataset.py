cd19701435a03d961a3639dfe86fb93d36d231cd
Gilad F <freidkin.gilad@gmail.com>
2024-02-06 22:44:54 +0200

    added data folder and moved some files there added data_structure.py file which has a basic data structure for each experiment in the project added ann_dataset.py file which has a basic structure of the dataset fot the project

---
+++ data/ann_dataset.py
@@ -0,0 +1,56 @@
+from __future__ import annotations
+import numpy as np
+import PIL.Image
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class ImageMeta:
+    path: str
+    size: tuple[int, int]
+    pixel_size: float
+
+    @staticmethod
+    def from_file(path: str, pixel_size: float) -> ImageMeta:
+        with PIL.Image.open(path) as image:
+            return ImageMeta(path, image.size, pixel_size)
+
+
+@dataclass(frozen=True)
+class Sample:
+    image: ImageMeta
+    bboxes: np.ndarray = None
+    keypoints: np.ndarray = None
+
+    def __post_init__(self):
+        have_bbox = self.bboxes is not None
+        have_keypoints = self.keypoints is not None
+
+        if have_bbox:
+            assert self.bboxes.shape[-1] == 4
+            assert 1 <= self.bboxes.ndim <= 2
+
+        if have_keypoints:
+            assert self.keypoints.dtype
+            assert self.keypoints.shape[-1] == 4
+            assert 1 <= self.keypoints.ndim <= 2
+
+        if have_bbox and have_keypoints:
+            assert self.bboxes.ndim == self.keypoints.ndim
+
+
+class AnnDataset:
+    def __init__(self, sample_list: list[Sample] = []) -> None:
+        self._sample_list: list[Sample] = sample_list
+
+    def __len__(self):
+        return len(self._sample_list)
+
+    def __getitem__(self, idx: int) -> Sample:
+        return self._sample_list[idx]
+
+    def add_sample(self, label: Sample):
+        self._sample_list.append(label)
+
+    def __iter__(self):
+        return self._sample_list.__iter__()
---
+++ data/bbox_utils.py
@@ -0,0 +1,75 @@
+import numpy as np
+from enum import Enum
+
+
+class BoxFormat(Enum):
+    XY_WH = 0
+    XY_XY = 1
+    XYmid_WH = 2
+
+
+class BoxUtils:
+    """
+    Helper methods for bbox manipulation
+    """
+
+    class Format(Enum):
+        XY_WH = 0
+        XY_XY = 1
+        XYmid_WH = 2
+
+    @staticmethod
+    def unpack(bbox: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        return np.split(bbox, bbox.shape[-1], axis=-1)
+
+    @staticmethod
+    def pack(c1: np.ndarray, c2: np.ndarray, c3: np.ndarray, c4: np.ndarray) -> np.ndarray:
+        return np.concatenate((c1, c2, c3, c4), axis=-1)
+
+    @staticmethod
+    def change_format(bbox: np.ndarray, src_format: Format, dst_format: Format) -> np.ndarray:
+        if src_format == dst_format:
+            return bbox
+
+        if (src_format, dst_format) == (BoxFormat.XY_XY, BoxFormat.XY_WH):
+            x1, y1, x2, y2 = BoxUtils.unpack(bbox)
+            w = x2 - x1
+            h = y2 - y1
+            return BoxUtils.pack(x1, y1, w, h)
+
+        elif (src_format, dst_format) == (BoxFormat.XY_XY, BoxFormat.XYmid_WH):
+            x1, y1, x2, y2 = BoxUtils.unpack(bbox)
+            w = x2 - x1
+            h = y2 - y1
+            xm = x1 + w // 2
+            ym = y1 + h // 2
+            return BoxUtils.pack(xm, ym, w, h)
+
+        elif (src_format, dst_format) == (BoxFormat.XY_WH, BoxFormat.XY_XY):
+            x1, y1, w, h = BoxUtils.unpack(bbox)
+            x2 = x1 + w
+            y2 = y2 + h
+            return BoxUtils.pack(x1, y1, x2, y2)
+
+        elif (src_format, dst_format) == (BoxFormat.XY_WH, BoxFormat.XYmid_WH):
+            x1, y1, w, h = BoxUtils.unpack(bbox)
+            xm = x1 + w // 2
+            ym = y1 + h // 2
+            return BoxUtils.pack(xm, ym, w, h)
+
+        elif (src_format, dst_format) == (BoxFormat.XYmid_WH, BoxFormat.XY_XY):
+            xm, ym, w, h = BoxUtils.unpack(bbox)
+            x1 = xm - w // 2
+            y1 = ym - h // 2
+            x2 = x1 + w
+            y2 = y1 + h
+            return BoxUtils.pack(x1, y1, x2, y2)
+
+        elif (src_format, dst_format) == (BoxFormat.XYmid_WH, BoxFormat.XY_WH):
+            xm, ym, w, h = BoxUtils.unpack(bbox)
+            x1 = xm - w // 2
+            y1 = ym - h // 2
+            return BoxUtils.pack(x1, y1, w, h)
+
+        else:
+            raise Exception("unsupported bbox format conversion.")
