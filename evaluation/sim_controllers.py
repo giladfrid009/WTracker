86122d3d57fc02d646f78378fc8abff270afaa26
Gilad F <freidkin.gilad@gmail.com>
2024-03-24 15:15:00 +0200

    some fixes and improvements

--- evaluation/sim_controllers.py
+++ evaluation/sim_controllers.py
@@ -5,19 +5,32 @@ import csv
 from dataclasses import dataclass, field
 from collections import deque
 
-from utils.path_utils import join_paths, create_directory
+from utils.path_utils import *
 from utils.io_utils import ImageSaver
 from evaluation.simulator import *
 from evaluation.simulator import Simulator
 
 
 class YoloController(SimController):
-    def __init__(self, config: TimingConfig, model: YOLO, device: str = "cpu", imgsz: int = 416):
+    def __init__(self, config: TimingConfig, model: YOLO, device: str = "cpu", **pred_kwargs: dict):
         super().__init__(config)
         self._camera_frames = deque(maxlen=1)
         self._model = model.to(device)
         self.device = device
-        self.imgsz = imgsz
+
+        self.pred_kwargs = self._format_kwargs(**pred_kwargs)
+
+    def _format_kwargs(self, **pred_kwargs) -> dict:
+        default_args = {
+            "imgsz": 416,
+            "conf": 0.1,
+            "verbose": False,
+        }
+
+        for key, val in default_args.items():
+            pred_kwargs.setdefault(key, val)
+
+        return pred_kwargs
 
     def on_sim_start(self, sim: Simulator):
         self._camera_frames.clear()
@@ -26,13 +39,14 @@ class YoloController(SimController):
         self._camera_frames.append(cam_view)
 
     def predict(self, *frames: np.ndarray) -> list[tuple[int, int, int, int]]:
+
         if len(frames) == 0:
             return []
 
         if frames[0].ndim == 2 or frames[0].shape[-1] == 1:
             frames = [cv.cvtColor(frame, cv.COLOR_GRAY2BGR) for frame in frames]
 
-        results = self._model.predict(frames, conf=0.1, device=self.device, imgsz=self.imgsz, max_det=1, verbose=False)
+        results = self._model.predict(frames, device=self.device, max_det=1, **self.pred_kwargs)
         results = [res.boxes.xywh[0].to(int).tolist() if len(res.boxes) > 0 else None for res in results]
         return results
 
@@ -52,33 +66,43 @@ class YoloController(SimController):
 @dataclass
 class LogConfig:
     root_folder: str
+    save_mic_view: bool = False
+    save_cam_view: bool = False
     mic_folder_name: str = "micro"
     cam_folder_name: str = "camera"
     err_folder_name: str = "errors"
     bbox_file_name: str = "bboxes.csv"
-    mic_folder_path: str = field(init=False)
-    cam_folder_path: str = field(init=False)
-    err_folder_path: str = field(init=False)
+    mic_file_name: str = "mic_{:09d}.png"
+    cam_file_name: str = "cam_{:09d}.png"
+
+    mic_file_path: str = field(init=False)
+    cam_file_path: str = field(init=False)
+    err_file_path: str = field(init=False)
     bbox_file_path: str = field(init=False)
-    save_mic_view: bool = False
-    save_cam_view: bool = False
 
     def __post_init__(self):
-        self.mic_folder_path = join_paths(self.root_folder, self.mic_folder_name)
-        self.cam_folder_path = join_paths(self.root_folder, self.cam_folder_name)
-        self.err_folder_path = join_paths(self.root_folder, self.err_folder_name)
+        self.mic_file_path = join_paths(self.root_folder, self.mic_folder_name, self.mic_file_name)
+        self.cam_file_path = join_paths(self.root_folder, self.cam_folder_name, self.cam_file_name)
+        self.err_file_path = join_paths(self.root_folder, self.err_folder_name, self.cam_file_name)
         self.bbox_file_path = join_paths(self.root_folder, self.bbox_file_name)
 
     def create_dirs(self):
-        create_directory(self.root_folder)
-        create_directory(self.err_folder_path)
-        create_directory(self.mic_folder_path)
-        create_directory(self.cam_folder_path)
+        create_parent_directory(self.mic_file_path)
+        create_parent_directory(self.cam_file_path)
+        create_parent_directory(self.err_file_path)
+        create_parent_directory(self.bbox_file_path)
 
 
 class LoggingController(YoloController):
-    def __init__(self, log_config: LogConfig, timing_config: TimingConfig, model: YOLO, device: str = "cpu", imgsz: int = 416):
-        super().__init__(timing_config, model, device, imgsz)
+    def __init__(
+        self,
+        log_config: LogConfig,
+        timing_config: TimingConfig,
+        model: YOLO,
+        device: str = "cpu",
+        **pred_kwargs: dict,
+    ):
+        super().__init__(timing_config, model, device, **pred_kwargs)
 
         self.log_config = log_config
         self._camera_frames = deque(maxlen=self.config.imaging_frame_num)
@@ -91,7 +115,7 @@ class LoggingController(YoloController):
         self._camera_frames.clear()
         self.log_config.create_dirs()
 
-        self._image_saver = ImageSaver(self.log_config.root_folder, tqdm=False)
+        self._image_saver = ImageSaver(tqdm=False)
         self._image_saver.start()
 
         self._bbox_file = open(self.log_config.bbox_file_path, "w")
@@ -128,12 +152,12 @@ class LoggingController(YoloController):
         self._camera_frames.append(cam_view)
 
         if self.log_config.save_cam_view:
-            path = join_paths(self.log_config.cam_folder_name, f"cam_{self._frame_number:09d}.png")
+            path = self.log_config.cam_file_path.format(self._frame_number)
             self._image_saver.schedule(cam_view, path)
 
     def on_micro_frame(self, sim: Simulator, micro_view: np.ndarray):
         if self.log_config.save_mic_view:
-            path = join_paths(self.log_config.mic_folder_name, f"mic_{self._frame_number:09d}.png")
+            path = self.log_config.mic_file_path.format(self._frame_number)
             self._image_saver.schedule(micro_view, path)
 
     def on_imaging_end(self, sim: Simulator):
@@ -148,7 +172,7 @@ class LoggingController(YoloController):
             if bbox is not None:
                 bbox = (bbox[0] + cam_pos[0], bbox[1] + cam_pos[1], bbox[2], bbox[3])
             else:
-                path = join_paths(self.log_config.err_folder_name, f"cam_{frame_number:09d}.png")
+                path = self.log_config.err_file_path.format(frame_number)
                 self._image_saver.schedule(self._camera_frames[i], path)
                 bbox = (-1, -1, -1, -1)
 
