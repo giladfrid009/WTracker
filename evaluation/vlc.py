109f409f9647c99ecd269840b71b99f0fcb3f125
TheGuy42 <guycohen801@gmail.com>
2024-05-13 16:25:31 +0300

    VLC can now show camera view and world view. VLC can now show boxes of prediction, micro and camera + shows center of micro Hotkeys can now have a description VLC prints the hotkeys and their description

--- evaluation/vlc.py
+++ evaluation/vlc.py
@@ -1,8 +1,9 @@
 from utils.path_utils import Files
-import cv2 as cv
+from dummy_reader import DummyReader
 
+import cv2 as cv
 from typing import Callable
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import matplotlib
 
 matplotlib.use("QTAgg")
@@ -20,6 +21,7 @@ from math import ceil, floor
 class HotKey:
     key: str
     func: Callable[[str], None]
+    description: str = field(default="")
 
     def __post_init__(self):
         self.key = self.key.lower()
@@ -31,7 +33,7 @@ class StreamViewer:
         self.window = None
         self.hotkeys: list[HotKey] = []
 
-        self.register_hotkey(HotKey("q", self.close))
+        self.register_hotkey(HotKey("q", self.close, "close the window"))
         self.set_title(self.window_name)
 
     def register_hotkey(self, hotkey: HotKey):
@@ -82,19 +84,25 @@ class StreamViewer:
 
 
 class VLC:
-    def __init__(self, files: Files, config: TimingConfig, log_path: str, cam_type: str = None) -> None:
+    def __init__(
+        self, files: Files, config: TimingConfig, log_path: str, cam_type: str = None
+    ) -> None:
         self.streamer = StreamViewer()
         self.index = 0
+        self._curr_row = None
         self.exit = False
         self.delay = 0
         self.play = False
         self.show_pred = False
+        self.show_micro = False
+        self.show_cam = False
 
+        self.cam_type: str = cam_type
         self.config: TimingConfig = config
-        self.reader = self._create_reader(files)
-        self.log = self._load_log(log_path)
-        self.cam_type = cam_type
-
+        self.log: pd.DataFrame = self._load_log(log_path)
+        self.reader: FrameReader = self._create_reader(files)
+        
+        
         self.initialize()
 
     def initialize(self):
@@ -106,21 +114,42 @@ class VLC:
         if log_path is None:
             return None
         log = pd.read_csv(log_path, index_col="frame")
-        assert len(log.index) == len(self.reader)
+        if self.cam_type == 'plt':
+            log['plt_x'] = 0
+            log['plt_y'] = 0
+            log['plt_h'] = max(log['cam_y']) + max(log['cam_h'])
+            log['plt_w'] = max(log['cam_x']) + max(log['cam_w'])
+        # assert len(log.index) == len(self.reader)
+        self._curr_row = log.iloc[self.index]
         return log
 
     def _init_hotkeys(self) -> None:
-        self.streamer.register_hotkey(HotKey("q", self.close))
-        self.streamer.register_hotkey(HotKey("d", self.next))
-        self.streamer.register_hotkey(HotKey("a", self.prev))
-        self.streamer.register_hotkey(HotKey("p", self.toggle_play))
-        self.streamer.register_hotkey(HotKey("h", self.toggle_pred))
+        self.streamer.register_hotkey(HotKey("q", self.close, "close VLC"))
+        self.streamer.register_hotkey(HotKey("d", self.next, "next frame"))
+        self.streamer.register_hotkey(HotKey("a", self.prev, "previous frame"))
+        self.streamer.register_hotkey(HotKey("p", self.toggle_play, "play/pause"))
+        self.streamer.register_hotkey(HotKey("h", self.toggle_pred, "toggle prediction box"))
+        self.streamer.register_hotkey(HotKey("m", self.toggle_micro, "toggle microscope box"))
+        self.streamer.register_hotkey(HotKey("c", self.toggle_cam, "toggle camera box"))
+
+    def print_hotkeys(self):
+        print("Hotkeys:")
+        for hotkey in self.streamer.hotkeys:
+            print(f" - {hotkey.key} : {hotkey.description}")
 
     def _create_window(self):
         self.streamer.create_trakbar("delay", 0, 250, self.set_delay)
         self.streamer.create_trakbar("#frame", 0, len(self.reader), self.seek)
 
     def _create_reader(self, files: Files) -> FrameReader:
+        if files is None:
+            frame_num = len(self.log.index)
+            frame_size = (
+                self.get_attribute(self.cam_type+"_h"),
+                self.get_attribute(self.cam_type+"_w"),
+            )
+            return DummyReader(frame_num, frame_size)
+
         filenames = [f for f in files]
         reader = FrameReader(files.root, filenames)
         return reader
@@ -142,17 +171,28 @@ class VLC:
         return title
 
     def get_attribute(self, col_name: str):
-        log_row = self.log.iloc[self.index]
-        return log_row[col_name]
+        # log_row = self.log.iloc[self.index]
+        # return log_row[col_name]
+        return self._curr_row[col_name]
+    
+    def update_curr_row(self):
+        self._curr_row = self.log.iloc[self.index]
 
     def get_photo(self) -> np.ndarray:
         photo = self.reader[self.index]
         if self.show_pred:
             self.add_pred(photo)
+        if self.show_micro:
+            self.add_micro_box(photo)
+        if self.show_cam:
+            self.add_cam_box(photo)
+
+        self.draw_center(photo)
         return photo
 
     def seek(self, pos: int):
         self.index = (pos) % len(self.reader)
+        self.update_curr_row()
         self.streamer.update(self.get_photo())
         self.streamer.set_title(self._get_title())
 
@@ -175,6 +215,12 @@ class VLC:
 
     def toggle_pred(self, key: str = None):
         self.show_pred = not self.show_pred
+    
+    def toggle_micro(self, key: str = None):
+        self.show_micro = not self.show_micro
+
+    def toggle_cam(self, key: str = None):
+        self.show_cam = not self.show_cam
 
     def mainloop(self):
         with self.streamer as streamer:
@@ -191,9 +237,15 @@ class VLC:
         h = self.get_attribute(prefix + "_h")
         return (x, y, w, h)
 
-    def add_pred(self, photo: np.ndarray) -> None:
+    def draw_box(
+        self,
+        photo: np.ndarray,
+        bbox: tuple[float, float, float, float],
+        color: tuple[int, int, int] = (0, 0, 255),
+        width: int = 1,
+    ) -> None:
         x, y, w, h = self.get_bbox(self.cam_type)
-        pred_x, pred_y, pred_w, pred_h = self.get_bbox("wrm")
+        pred_x, pred_y, pred_w, pred_h = bbox
 
         pred_x = floor(pred_x - x)
         pred_y = floor(pred_y - y)
@@ -201,11 +253,57 @@ class VLC:
         pred_h = ceil(pred_h)
 
         if photo.shape[0] <= pred_x or photo.shape[1] <= pred_y:
-            print(f"Warning::pred = ({pred_x}, {pred_y}, {pred_w}, {pred_h}) # cam = ({x}, {y}, {w}, {h})")
+            print(
+                f"Warning::draw_box = ({pred_x}, {pred_y}, {pred_w}, {pred_h}) # cam = ({x}, {y}, {w}, {h})"
+            )
             return
 
-        pred_x = max(pred_x, 0)
-        pred_y = max(pred_y, 0)
-        pred_w = min(pred_w, w)
-        pred_h = min(pred_h, h)
-        cv.rectangle(photo, (pred_x, pred_y), (pred_x + pred_w, pred_y + pred_h), (0, 0, 255), 1)
+        # pred_x = max(pred_x, 0)
+        # pred_y = max(pred_y, 0)
+        # pred_w = min(pred_w, w)
+        # pred_h = min(pred_h, h)
+        cv.rectangle(
+            photo, (pred_x, pred_y), (pred_x + pred_w, pred_y + pred_h), color, width
+        )
+    
+    def draw_marker(
+        self,
+        photo: np.ndarray,
+        x: float,
+        y: float,
+        marker_size: int = 5,
+        marker_type: int = cv.MARKER_CROSS,
+        color: tuple[int, int, int] = (0, 0, 255),
+        thickness: int = 1,
+    ) -> None:
+        frame_x, frame_y, frame_w, frame_h = self.get_bbox(self.cam_type)
+        x, y = floor(x-frame_x), floor(y-frame_y)
+        cv.drawMarker(photo, (x, y), color, marker_type, marker_size, thickness)
+
+    def draw_center(self, photo:np.ndarray):
+        x, y, w, h = self.get_bbox('mic')
+        center = (x + w // 2, y + h // 2)
+        cv.drawMarker(photo, center, (0, 0, 255), cv.MARKER_CROSS, 7, 1)
+
+    def add_pred(self, photo: np.ndarray) -> None:
+        # x, y, w, h = self.get_bbox(self.cam_type)
+        # pred_x, pred_y, pred_w, pred_h = self.get_bbox("wrm")
+        worm_bbox = self.get_bbox("wrm")
+        self.draw_box(photo, worm_bbox, (0, 0, 0), 1)
+
+    def add_micro_box(self, photo: np.ndarray) -> None:
+        mic_bbox = self.get_bbox("mic")
+        self.draw_box(photo, mic_bbox, (0, 0, 255), 2)
+
+    def add_cam_box(self, photo: np.ndarray) -> None:
+        cam_bbox = self.get_bbox("cam")
+        self.draw_box(photo, cam_bbox, (128, 0, 0), 2)
+
+
+
+
+
+
+
+
+
